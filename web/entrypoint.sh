8348fc3decd4c2f83e1ee33a21cba9e9736a4482
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-09-13 23:22:29 +0400

    docker

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -2,3 +2,7 @@ APP_PORT=${PORT:8000}
 cd /app/
 /opt/venv/Scripts/gunicorn --worker-tmp-dir /dev/shm django_k8s.wsgi:application 
 --bind "0.0.0.0:${APP_PORT}"
+
+#or
+# /opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm django_k8s.wsgi:application 
+# --bind "0.0.0.0:${APP_PORT}"
